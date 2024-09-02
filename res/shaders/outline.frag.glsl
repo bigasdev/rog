e2da93a8bb71dc038d69943b2999d69a27bb4fee
gustavo domingues <deathdiec@gmail.com>
2024-09-02 17:27:10 -0300

    something something

--- res/shaders/outline.frag.glsl
+++ res/shaders/outline.frag.glsl
@@ -5,8 +5,8 @@ in vec2 texCoord;
 out vec4 fragColor;
 
 uniform sampler2D tex;
-uniform vec4 outlineColor = vec4(1.0, 1.0, 1.0, 1.0); // Default white color
-uniform float outlineThickness = 0.00275; // Default thickness of 1
+uniform vec4 outlineColor = vec4(1.0, 1.0, 0.0, 1.0); // Default white color
+uniform float outlineThickness = 0.00295; // Default thickness of 1
 
 void main(void)
 {
@@ -18,7 +18,7 @@ void main(void)
   alpha = max(alpha, texture2D(tex, texCoord + vec2(0, outlineThickness)).a);
   alpha = max(alpha, texture2D(tex, texCoord + vec2(0, -outlineThickness)).a);
 
-  if(alpha > 0.0 && color_frag.a == 0.0){
+  if(color_frag.a == 0.0 && alpha >= 1.0){
       fragColor.rgb = outlineColor.rgb;
       fragColor.a = alpha;
     }else{
